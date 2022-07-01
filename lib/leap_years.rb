3f1e0da4f91e1651870ca598d4e262e22e99371e
Ruzeka Uddin <ruzekauddin@hotmail.co.uk>
2022-07-01 11:01:23 +0100

    Added second test/method passed test

--- lib/leap_years.rb
+++ lib/leap_years.rb
@@ -1,3 +1,10 @@
-def leap_years
+def leap_years?(year)
+
+  if year % 400 == 0
+    return true
+
+  elsif year % 100 == 0 && year % 400 != 0
+    return false
+  end 
   
 end
--- spec/leap_years_spec.rb
+++ spec/leap_years_spec.rb
@@ -5,4 +5,12 @@ describe " leap_years? " do
   it "should return true if the years divisible by 400" do
   expect(leap_years?(2000)).to eq true
   end
+
+  it " should return false if all years divisible by 100 but not 400" do
+    expect(leap_years?(1700)).to eq false
+  end
 end
+
+# Return any by 100 but not 400 should be false
+# All years divisible by 100 but not by 400 are 
+# NOT leap years (e.g. 1700, 1800 and 1900 were not leap years)
